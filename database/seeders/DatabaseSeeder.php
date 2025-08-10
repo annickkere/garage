29b4ab71680d8b8359cc3e394d87148f7756f4f0
Annick Kere <annickkere@gmail.com>
2025-08-10 15:29:05 +0000

    Refractoring Database Seeder file

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,22 +2,21 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use Illuminate\Database\Seeder;
+use App\Models\Vehicule;
+use App\Models\Reparation;
+use App\Models\Technicien;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
+        Vehicule::factory(10)->create();
+        Technicien::factory(5)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        Reparation::factory(15)->create()->each(function ($reparation) {
+            $reparation->techniciens()->attach(
+                Technicien::inRandomOrder()->take(rand(1, 3))->pluck('id')
+            );
+        });
     }
 }
