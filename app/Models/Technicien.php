50af2381a7212c8e559ad4b97ec5c59d56e09e57
Annick Kere <annickkere@gmail.com>
2025-08-10 14:32:24 +0000

    Refractoring Technicien Model

--- app/Models/Technicien.php
+++ app/Models/Technicien.php
@@ -3,8 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Technicien extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['nom', 'prenom', 'specialite'];
+
+    public function reparations()
+    {
+        return $this->belongsToMany(Reparation::class);
+    }
 }
