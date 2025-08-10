504f00ac036e24b30b953a84f8632668b4dc99be
Annick Kere <annickkere@gmail.com>
2025-08-10 14:17:40 +0000

    Refractoring Vehicule Model

--- app/Models/Vehicule.php
+++ app/Models/Vehicule.php
@@ -3,8 +3,20 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Vehicule extends Model
 {
-    //
+   use HasFactory;
+
+   protected $fillable = [
+    'immatriculation',
+    'marque',
+    'modele',
+    'couleur',
+    'annee',
+    'kilometrage',
+    'carosserie',
+    'energie',
+    'boite'];
 }
